237780de64eb39475f6d8c0dbb9ffc5a9f26d9a4
vyshnavi0106 <72871125+vyshnavi0106@users.noreply.github.com>
2021-05-10 11:35:55 +0530

    Add files via upload

---
+++ vyshnavipy.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 1.5
+>>> num2=2
+>>> sum=num1+num2
+>>> print('the sum is'.format(num1,num2,sum))
+the sum is
+>>> 
